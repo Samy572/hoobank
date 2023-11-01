86a6f5e3c7c43aafc9cec3c5bd7adc80b601ec42
Sam <samy_ch@outlook.fr>
2023-11-02 00:00:45 +0100

    💄 Update responsive footer

--- src/app/Feature/listfeature.tsx
+++ src/app/Feature/listfeature.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ListFeature = ({ img, title, text }: Props) => {
 	return (
-		<div className="md:p-6  p-4 mt-10 grid grid-cols-[70px,1fr] justify-center gap-x-3  hover:bg-gradient-to-r from-[#272525] to-slate-900 rounded-xl ">
+		<div className="md:p-6  p-4 mb-10 grid grid-cols-[70px,1fr] justify-center gap-x-3  hover:bg-gradient-to-r from-[#272525] to-slate-900 rounded-xl ">
 			<div className=" w-[60px] h-[60px] flex justify-center rounded-full  bg-sahdowBlue">
 				<Image src={img} width={30} height={30} alt="hero" />
 			</div>
--- src/app/Footer/footer.tsx
+++ src/app/Footer/footer.tsx
@@ -1,4 +1,5 @@
 import List from './list';
+import Listfooter from './listfooter';
 import Socialmedia from './socialmedia';
 
 const Footer = () => {
@@ -13,18 +14,17 @@ const Footer = () => {
 	return (
 		<div>
 			<List />
-			<div className="mx-6 sm:px-16 mt-6 border-t border-gray-400">
-				<div className="py-4 text-center text-lg">
+			<div className="align-middle mx-6 sm:px-16 mt-6 border-t border-gray-400">
+				<div className=" w-full  lg:flex lg:justify-between  lg:text-left py-4 text-center text-lg">
 					<h4 className="text-white">
 						Copyright © {year} HooBank. All <br /> Rights Reserved.
 					</h4>
+					<ul className="flex justify-center  ">
+						{Object.entries(listsocial).map(([key, value]) => {
+							return <Socialmedia key={key} img={value} />;
+						})}
+					</ul>
 				</div>
-				<ul className="flex justify-center">
-					{Object.entries(listsocial).map(([key, value]) => {
-						return <Socialmedia key={key} img={value} />;
-					})}
-				</ul>
-				L
 			</div>
 		</div>
 	);
